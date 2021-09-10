4b859cf799abc835523d175b30e05b88ac78eb27
Kaitlin <kaitlin@Kaitlins-MacBook-Pro.local>
2021-09-10 16:56:28 +1000

    here is our reactApp

--- src/App.test.js
+++ src/App.test.js
@@ -2,4 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  render(<App />);
+  const text = screen.getByText("This is a react Class");
+  expect(text).toBeInTheDocument();
 });
